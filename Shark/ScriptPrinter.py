6f898135a46fc45e5612f5229c561a21c685bde7
Shark Kim <ch96an@gmail.com>
2022-01-01 17:19:15 +0900

    blacked codes

--- Shark/ScriptPrinter.py
+++ Shark/ScriptPrinter.py
@@ -3,21 +3,23 @@ from RREF_Decomposition import *
 from MatrixUtils import *
 from fractions import Fraction
 
+
 def ScriptPrinter():
-  n, m, r, r1, r2, lb, ub = 6, 8, 4, 5, 3, 16, 2500
-  matrix = generateMatrixWithIntegerRREF(n, m, r, r1, r2, lb, ub)
-  matrix = [list(map(Fraction,row)) for row in matrix]
+    n, m, r, r1, r2, lb, ub = 6, 8, 4, 5, 3, 16, 2500
+    matrix = generateMatrixWithIntegerRREF(n, m, r, r1, r2, lb, ub)
+    matrix = [list(map(Fraction, row)) for row in matrix]
 
-  opList, rref = GaussJordanDecomposition(matrix)
+    opList, rref = GaussJordanDecomposition(matrix)
 
-  print(n,m)
-  printMatrix(matrix)
-
-  for op in opList:
-    print()
-    print(op)
-    op.applyTo(matrix)
+    print(n, m)
     printMatrix(matrix)
 
-if __name__ == '__main__':
-  ScriptPrinter()
+    for op in opList:
+        print()
+        print(op)
+        op.applyTo(matrix)
+        printMatrix(matrix)
+
+
+if __name__ == "__main__":
+    ScriptPrinter()
--- Shark/ScriptWriter.py
+++ Shark/ScriptWriter.py
@@ -1,10 +1,10 @@
-f = open('Script', 'w')
+f = open("Script", "w")
 
 while True:
-  try:
-    line = input()
-    f.write(line)
-    f.write('\n') 
-  except:
-    f.close()
-    break
+    try:
+        line = input()
+        f.write(line)
+        f.write("\n")
+    except:
+        f.close()
+        break
